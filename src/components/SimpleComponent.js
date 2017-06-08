8a3aa751c833d9f03d50b7b5fb4b94affb01b782
github username <github email address>
2017-06-08 16:36:18 -0400

    tests pass but no render

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
 // Code SimpleComponentHere Here
+import React from 'react'
+import { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+  constructor (){
+    super()
+    this.state= {
+      mood: 'happy'
+    }
+        this.handleClick = this.handleClick.bind(this)
+       }
+
+       handleClick() {
+         this.setState({
+           mood: 'sad'
+         })
+
+       }
+       render() {
+         return <div onClick={this.handleClick}>
+         {this.state.mood}
+         </div>
+       }
+     }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={alert("on the inside")} />
   </div>,
   document.getElementById('root')
 );
